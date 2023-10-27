b9130d1827c0624ae2b2668f2e2c9039b497c4eb
hm <keg9317@naver.com>
2023-10-27 17:33:04 +0900

    레이아웃 수정

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+//입력값 체크 후 총 금액 표시하기 (여러개.. 값 어떻게 체크?)
+const input = document.querySelectorAll(".myScale");
+console.log(input);
 
+// input.addEventListener("keyup", (e) => {
+//   //   const myTotalCount = document.querySelector(".totalCount");
+//   //   myTotalCount.setAttribute("value", 300);
+//   console.log(e);
+// });
